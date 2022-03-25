db03931593e08ffc0d9fafd2222a1e549a533d5d
sjkang930 <91238446+sjkang930@users.noreply.github.com>
2022-03-24 17:08:11 -0700

    minor bug fixed

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,6 +1,6 @@
 const is_heroku = process.env.IS_HEROKU || false;
 
-const dbConfigHeroku = "mongodb+srv://theMongoAdmin:<password>@cluster0.kutaa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const dbConfigHeroku = "mysql://b1ab7fb2ee03bc:2a484a2d@us-cdbr-east-03.cleardb.com/heroku_3d208ad4bd6f421";
 
 const dbConfigLocal = "mysql://root:Password@localhost/lab_example";
 
